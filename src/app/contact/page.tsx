40ed3738cb5f0e985ca0c9da9f390c47ae709216
Alessandro <alessandromasiero158@libero.it>
2024-10-14 21:25:34 +0200

    syntax error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import Layout from "../_core/components/Layout";
 
 export default function ContactScreen() {
   return (
-    <Layout currentPath="Contct">
+    <Layout currentPath="Contact">
       <div>
         <h1>Contact Page</h1>
       </div>
