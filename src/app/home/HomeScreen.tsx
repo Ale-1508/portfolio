eb7532e221c06a9aab2914027001b90016995e5a
Alessandro <alessandromasiero158@libero.it>
2024-10-21 22:35:28 +0200

    Added WorkInProgress section to homepage

--- src/app/home/HomeScreen.tsx
+++ src/app/home/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import WhoAmI from "./components/WhoAmI";
+import WorkInProgress from "./components/WorkInProgress";
 
 
 function HomeScreen() {
   return(
     <>
       <WhoAmI />
+      <WorkInProgress />
     </>
   );
 }
