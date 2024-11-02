90de775c1250fb70599ca4155257127275043ac8
Alessandro <alessandromasiero158@libero.it>
2024-11-02 21:46:30 +0100

    Underline class tailwind green-500

--- src/app/_core/components/Layout.tsx
+++ src/app/_core/components/Layout.tsx
@@ -10,7 +10,7 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children, currentPath } ) => {
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 selection:bg-primary-500 selection:text-primary-50">
       <Navbar currentPath={currentPath}/>
       {children}
     </div>
