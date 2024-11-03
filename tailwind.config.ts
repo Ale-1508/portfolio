52e4edcbeefe65e8e0aee62b08392c5ee5018089
Alessandro <alessandromasiero158@libero.it>
2024-11-03 12:25:41 +0100

    added more border radius and width options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,14 @@ const config: Config = {
         '4xl': '2rem',
         '5xl': '2.5rem',
         '6xl': '3rem',
+        '7xl': '3.5rem',
+        '8xl': '4rem',
+        '9xl': '4.5rem',
+        '10xl': '5rem',
+      },
+      width: {
+        '128': '32rem',
+        '35vw': '35vw',
       },
       boxShadow: {
         'elevation-5': '0 5px 15px rgba(0, 0, 0, 0.3)',
