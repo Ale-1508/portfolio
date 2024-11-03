88b9eb08d726b9d3c8f36507715187d145c3b33b
Alessandro <alessandromasiero158@libero.it>
2024-11-03 18:32:44 +0100

    Added windows resize handler to trigger the breakpoint medium-large for the TimeLine components

--- src/app/about/components/TimeLine.tsx
+++ src/app/about/components/TimeLine.tsx
@@ -37,24 +37,47 @@ const getLastDigit = (number: number) => number % 10;
 
 const TimeLine = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
 
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
+  
   useEffect( () => {
     setIsVisible(true);
   }, [])
   
   return (
-    <div className={`grid grid-cols-4 gap-y-4 
+    <div className={`grid grid-cols-1 ml:grid-cols-4 gap-y-4
       transition-transform duration-700 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
     `}>
       {experiences.map((experience) => {
         const dateDifference = getDateDifference(experience.from, experience.to, experience.onGoing);
-        
+        const containerStyle = windowSize.width >= 896
+          ? {gridColumn: `${getLastDigit(experience.from) + 1} / span ${dateDifference}`}
+          : {}
+
         return (
           <div
             key={experience.id}
-            style={{gridColumn: `${getLastDigit(experience.from) + 1} / span ${dateDifference}`}}
+            style={containerStyle}
             className="
-              flex justify-center md:justify-between items-center
+              timeline-media 
+              flex justify-between items-center
               rounded-4xl py-4 px-8
               bg-primary-500 text-white
               selection:bg-primary-300 cursor-pointer
@@ -62,13 +85,13 @@ const TimeLine = () => {
           ">
             <div className='flex flex-col gap-2'>
               <h2 className='font-semibold
-                text-lg md:text-xl lg:text-2xl
+                text-xl lg:text-2xl
               '>{experience.company}</h2>
               <h3 className=' font-normal
-                text-sm md:text-md lg:text-lg
+                text-md lg:text-lg
               '>{experience.role}</h3>
             </div>
-            <h1 className='text-3xl font-semibold hidden ml:inline'>{dateDifference}{ experience.onGoing ? "+" : ""}</h1>
+            <h1 className='text-3xl font-semibold'>{dateDifference}{ experience.onGoing ? "+" : ""}</h1>
           </div>
         );
       })}
