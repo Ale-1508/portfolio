a52af0ca5b35c2c4a063661134350c089dc5ab97
Alessandro <alessandromasiero158@libero.it>
2024-11-03 12:26:03 +0100

    added classname prop to Layout component

--- src/app/_core/components/Layout.tsx
+++ src/app/_core/components/Layout.tsx
@@ -1,19 +1,22 @@
 import React, { ReactNode } from 'react';
+import classNames from 'classnames'
 
 import Navbar from './Navbar';
-import GlobalStateProvider from '../GlobalStateContext';
 
 type LayoutProps = {
   children: ReactNode;
   currentPath: string;
+  className?: string;
 };
 
-const Layout: React.FC<LayoutProps> = ({ children, currentPath } ) => {
+const Layout: React.FC<LayoutProps> = ({ children, currentPath, className } ) => {
   return (
-    <div className="flex flex-col gap-16 
-    font-sans
-    selection:bg-primary-500 selection:text-primary-50
-    ">
+    <div className={classNames(
+      "flex flex-col gap-16 font-sans", 
+      "selection:bg-primary-500 selection:text-primary-50",
+      className
+    )}
+    >
       <Navbar currentPath={currentPath}/>
       {children}
     </div>
