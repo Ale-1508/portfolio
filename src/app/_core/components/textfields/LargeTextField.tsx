c33506037df81ecc0572126236d34ec572d7b8bb
Alessandro <alessandromasiero158@libero.it>
2024-11-08 22:38:21 +0100

    added word counter

--- src/app/_core/components/textfields/LargeTextField.tsx
+++ src/app/_core/components/textfields/LargeTextField.tsx
@@ -11,70 +11,57 @@ interface SmallTextfieldProps {
 }
 
 export const LargeTextField = ( { startValue="", hint="", required=false }: SmallTextfieldProps ) => {
-  const [ state, setState ] = useState({
-    value: startValue,
-    trailingIcon: {
-      name: faCircleInfo,
-      show: false
-    }
-  });
+  const [ value, setState ] = useState(startValue);
+  const valueLenght = value.length;
 
   const capitalizeFirstLetter = (string: string) => { return string.charAt(0).toUpperCase() + string.slice(1); };
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setState(prevState => ({...prevState, value:e.target.value}));
+    setState( e.target.value );
   }
   
-  const handleFocus = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setState(prevState => ({
-      ...prevState, 
-      trailingIcon: { ...prevState.trailingIcon, show: false, name: faCircleInfo }
-    }));
-  }
+  const handleFocus = (e: ChangeEvent<HTMLTextAreaElement>) => {}
   
-  const handleBlur = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    let inputOk = true;
-    if (inputOk) {
-      setState(prevState => ({
-        ...prevState, 
-        trailingIcon: { ...prevState.trailingIcon, show: true, name: faCheck }
-      }));
-    } else {
-      setState(prevState => ({
-        ...prevState, 
-        trailingIcon: { ...prevState.trailingIcon, show: true }
-      }));
-    }
-  }
+  const handleBlur = (e: ChangeEvent<HTMLTextAreaElement>) => {}
 
   
   return (
     <div className="
-      flex flex-row items-start h-48
-      px-4 py-2 rounded-2xl
-      text-md lg:text-lg font-normal 
-      text-primary-600
-      bg-white
-      selectable-none hover:shadow-md
-      focus-within:outline-primary-300 focus-within:outline-2 focus-within:outline
-    ">
-      <textarea 
-        value={state.value}
-        placeholder={hint}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        required={required}
-        className="
-          py-2 w-full h-full
-          bg-transparent
-          text-md lg:text-lg font-normal 
-          text-primary-600
-          placeholder-primary-400 placeholder-font-normal
-          focus:outline-none
-          overflow-hidden resize-none
-        "
-      />
+      flex flex-col gap-2
+      text-primary-600 text-md lg:text-lg font-normal ">
+      <div className="
+        flex flex-col items-start h-48
+        px-4 py-2 rounded-2xl
+        
+        bg-white
+        selectable-none hover:shadow-md
+        focus-within:outline-primary-300 focus-within:outline-2 focus-within:outline
+      ">
+        <textarea 
+          value={value}
+          placeholder={hint}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          required={required}
+          className="
+            py-2 w-full h-full
+            bg-transparent
+            text-md lg:text-lg font-normal 
+            text-primary-600
+            placeholder-primary-400 placeholder-font-normal
+            focus:outline-none
+            overflow-hidden resize-none
+          "
+        />
+        
+      </div>
+      { <p className={`
+        transition duration-500 ease-out ${ valueLenght < 20 ? "opacity-100" : "opacity-0" }
+        w-full flex -ml-1
+        justify-end items-end 
+        font-medium text-sm
+      `}>{valueLenght}/20</p> }
     </div>
   )
 }
