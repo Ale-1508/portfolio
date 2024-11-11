ab0165282eb83eb309ee152a26029b6482396dcf
Alessandro <alessandromasiero158@libero.it>
2024-11-11 19:28:52 +0100

    added showcase header

---
+++ src/app/showcase/components/Header.tsx
@@ -0,0 +1,15 @@
+interface HeaderProps { 
+  title:string
+  isVisible: boolean
+}
+
+export const Header = ( { title, isVisible } : HeaderProps ) => {
+  return (
+    <h1 className={`
+      text-4xl lg:text-5xl 
+      font-semibold leading-tight
+      text-primary-600
+      transition-transform duration-300 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
+    `}>{title}</h1>
+  )
+}
--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import Layout from "../_core/components/Layout";
 
+import classNames from "classnames";
+import { useEffect, useState } from "react"
+import { Header } from "./components/header";
+
 export default function ShowcaseScreen() {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect( () => {
+    setIsVisible(true);
+  }, [])
+
   return (
     <Layout currentPath="Showcase">
-      <div>
-        <h1>Showcase Page</h1>
+      <div className={`
+        flex flex-col gap-8 justify-center items-center
+        transition-transform duration-500 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
+        `}>
+        <Header title="Project Showcase" isVisible={isVisible}></Header>
       </div>
     </Layout>
   );
