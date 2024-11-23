e0fee7909eddb10836e226d68d73bd29f7690ca3
Alessandro <alessandromasiero158@libero.it>
2024-11-23 19:39:29 +0100

    added suspense for search params

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import Layout from "../_core/components/Layout";
 import { useEffect, useState } from "react"
+import { Suspense } from "react";
+
+import Layout from "@/components/Layout";
 import { Header } from "@/components/Header";
 import { useSearchParams } from 'next/navigation'
 import ExperienceList from "./components/ExperienceList";
 import ExperienceDetails from "./components/ExperienceDetails";
 
-export default function ShowcaseScreen() {
-
+const ShowcaseScreenBody = () => {
   const searchParams = useSearchParams()
   const workExperienceID = searchParams.get('id')
   // URL -> `/experience?id=my-project-id`
@@ -21,16 +22,25 @@ export default function ShowcaseScreen() {
 
   return (
     <Layout currentPath="" className="mx-8 sm:mx-16 md:mx-24 lg:mx-32 gap-4 mb-16">
-      <div className={`
-        flex flex-col gap-16 justify-center items-center
-        transition-transform duration-500 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
-        `}>
-        <Header title="My Experience" isVisible={isVisible}></Header>
-        {workExperienceID
-          ? <ExperienceDetails id={workExperienceID}/>
-          : <ExperienceList />
-        }
-      </div>
-    </Layout>
+        <div className={`
+          flex flex-col gap-16 justify-center items-center
+          transition-transform duration-500 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
+          `}>
+          <Header title="My Experience" isVisible={isVisible}></Header>
+          
+            {workExperienceID
+              ? <ExperienceDetails id={workExperienceID}/>
+              : <ExperienceList />
+            }
+        </div>
+      </Layout>
+  )
+}
+
+export default function ShowcaseScreen() {
+  return (
+    <Suspense fallback={<div className="text-lg">Experiences Loading...</div>}>
+      <ShowcaseScreenBody />
+    </Suspense>
   );
 }
