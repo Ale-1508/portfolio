d5dfa76b0edc8587c3a24587ed55e8d3da83870b
Alessandro <alessandromasiero158@libero.it>
2024-11-23 17:32:53 +0100

    Fixed Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Layout from "./_core/components/Layout";
 
 export default function Home() {
   return (
-    <Layout currentPath="Work" className="mx-8 sm:mx-16 md:mx-24 lg:mx-32 gap-12 mb-16">
-      <HomeScreen />
-    </Layout>
+    <HomeScreen />
   );
 }
