fefc67663d275b97cfd1e3c8039614ab5b7a8e2c
Alessandro <alessandromasiero158@libero.it>
2024-11-24 11:41:39 +0100

    Updated showcase responsive layout

--- src/app/_core/components/Header.tsx
+++ src/app/_core/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 export const Header = ( { title, isVisible } : HeaderProps ) => {
   return (
     <h1 className={`
-      text-4xl lg:text-5xl 
+      text-3xl sm:text-4xl lg:text-5xl 
       font-semibold leading-tight
       text-primary-600
       transition-transform duration-300 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}
